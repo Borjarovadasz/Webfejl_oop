1594f151c70c169e5589b8f2ce2263a3c1145057
Meszaros Nandor Farkas <meszaros.nandor1@BOLYAI.LOCAL>
2024-11-29 12:47:41 +0100

    going to jump

---
+++ basics/script.js
@@ -0,0 +1,29 @@
+function Player(nickname) {
+    this.nickname = nickname
+    this.playedmatch = 0
+}
+
+Player.prototype.play = function() {
+    this.playedmatch++
+    console.log(this.nickname, this.playedmatch);
+    }
+    
+Player.prototype.getTierlevel = function() {
+    if(this.playedmatch <= 3)
+    {
+        return 'a' 
+    }else if(this.playedmatch >3 && this.playedmatchs <=6) {
+        return 'b'
+    }else if(this.playedmatch > 6) {
+        return 'c'
+    }
+    
+
+}
+
+const gomszab = new Player("gomszab")
+
+console.log(gomszab)
+
+gomszab.play()
+console.log(gomszab.getTierlevel())
