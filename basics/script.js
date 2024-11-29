86ac7f37f1921a50fe845d75136442691b25aecc
Meszaros Nandor Farkas <meszaros.nandor1@BOLYAI.LOCAL>
2024-11-29 12:58:31 +0100

    Közeledek a talajhoz

--- basics/script.js
+++ basics/script.js
@@ -1,25 +1,51 @@
-function Player(nickname) {
-    this.nickname = nickname
-    this.playedmatch = 0
-}
+// function Player(nickname) {
+//     this.nickname = nickname
+//     this.playedmatch = 0
+// }
 
-Player.prototype.play = function() {
-    this.playedmatch++
-    console.log(this.nickname, this.playedmatch);
+// Player.prototype.play = function() {
+//     this.playedmatch++
+//     console.log(this.nickname, this.playedmatch);
+//     }
+    
+// Player.prototype.getTierlevel = function() {
+//     if(this.playedmatch <= 3)
+//     {
+//         return 'a' 
+//     }else if(this.playedmatch >3 && this.playedmatchs <=6) {
+//         return 'b'
+//     }else if(this.playedmatch > 6) {
+//         return 'c'
+//     }
+    
+
+// }
+
+class Player {
+    constructor(nickname) {
+        this.nickname = nickname
+        this.playedmatch = 0;
+    }
+    play() {
+        this.playedmatch++;
+       console.log(this.nickname, this.playedmatch);
+       
     }
     
-Player.prototype.getTierlevel = function() {
+getTierlevel() {
     if(this.playedmatch <= 3)
-    {
-        return 'a' 
-    }else if(this.playedmatch >3 && this.playedmatchs <=6) {
-        return 'b'
-    }else if(this.playedmatch > 6) {
-        return 'c'
-    }
-    
-
+         {
+          return 'a' 
+         }else if(this.playedmatch >3 && this.playedmatchs <=6) {
+             return 'b'
+            }else if(this.playedmatch > 6) {
+              return 'c'
+           }
 }
+}
+
+
+
 
 const gomszab = new Player("gomszab")
 
