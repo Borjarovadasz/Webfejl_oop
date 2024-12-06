213fc3a7ec5d8c5d4fdfe5541ebebe27552ef345
Meszaros Nandor Farkas <meszaros.nandor1@BOLYAI.LOCAL>
2024-12-06 12:58:20 +0100

    elso

---
+++ santa/classes.js
@@ -0,0 +1,43 @@
+class Factory{
+    constructor()
+    {
+        this.manolist = []  //Létrehozzuk manólista tulajdnságot, üres liste mivel az új gyárhoz nem tartozik manó, majd ha adunk hozzá manót ehhez fogjuk mehívni.
+    }
+
+    addmano(mano) { // Mano paraméter mert a mano lista elérhető az osztályon belül vagyunk.
+
+        this.manolist.push(mano)
+        createRow(mano)
+    }
+ // TODO 1, 2, 3, 4, 9, 10
+}
+/*
+id , keresztnev, vezeteknev, reszleg
+*/
+
+class Companion{
+    constructor(id, keresztnev, vezetknev, reszleg)
+    {
+    this.id = id
+    this.keresztnev = keresztnev
+    this.vezetknev = vezetknev
+    this.reszleg = reszleg
+    this.productlist = [] // Nem kötelező megadni ezért nem kell a propretybe írni de kell mivel ez lesz az odalon le üresen kezdődik
+    }
+
+    getname() {
+
+    return  this.keresztnev + " " + this.vezetknev  //Azért igy mert a class tulajdonságaira hivatkozunk
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+    }
+
+    productadd(product) 
+    {
+        this.productlist.push(product)
+    } 
+
+}
+
+ // TODO 5
+
+
---
+++ santa/main.js
@@ -0,0 +1,77 @@
+/**
+ * @type {{
+ *   firstName: string;
+ *   lastName: string;
+ *   products: string[];
+ *   area: string;
+ * }[]}
+ */
+const companionList = [
+    {
+        firstName: 'Géza',
+        lastName: 'Kiss',
+        area: 'plastic',
+        products: ['kisauto', 'barbibaba']
+    },
+    {
+        firstName: 'Ferenc',
+        lastName: 'Tóth',
+        area: 'paper',
+        products: ['könyv']
+    },
+    {
+        firstName: 'Gábor',
+        lastName: 'Nagy',
+        area: 'plastic',
+        products: ['zokni']
+    },
+]
+
+const factory = new Factory();
+
+
+
+document.getElementById('companion').addEventListener('submit',function(e){
+    e.preventDefault();
+    const form =  e.currentTarget
+    addCompanion(form, factory);
+});
+
+document.getElementById('product').addEventListener('submit',function(e){
+    e.preventDefault();
+    const form = e.currentTarget;
+    addProductForm(form, factory)
+});
+
+/**
+ * table render
+ */
+function initTable(){
+    for(let i = 0; i < companionList.length; i++) {
+        const currentelement = companionList[i]
+        const companion = new Companion(i,currentelement.firstName,currentelement.lastName,currentelement.area,currentelement.products) //(i az index a currentelement pedig megnézi az adott elemnek a NEVÉT az AREÁJÁ és a PRODUKTJÁT)
+        factory.addmano(companion)
+        for(const produce of currentelement.products){
+            companion.productadd(produce)
+            
+        }
+    }
+
+    console.log(factory)
+    // TODO 6
+}
+
+
+initTable()
+
+/**
+ * 
+ * eventlistener callback for the button click of companion
+ * 
+ * @param {EventTarget} e 
+ */
+function checkEventListener(e){
+    const row = e.currentTarget.parentElement.parentElement;
+    const companionId = row.id;
+    // TODO 10
+}
