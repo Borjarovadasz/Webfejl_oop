6a7bef1302f018577ea0dcb76c5d417ea1b3609d
Meszaros Nandor Farkas <meszaros.nandor1@BOLYAI.LOCAL>
2024-12-13 12:38:33 +0100

    orai vandalkodasok

--- table/scripts.js
+++ table/scripts.js
@@ -19,3 +19,51 @@ const array = [
         lastname: 'Horváth'
     },
 ]
+
+class Person {
+    constructor(obj) {
+        this.firstname1 = obj.firstname1,
+        this.firstname2 = obj.firstname2,
+        this.lastname = obj.lastname
+    }
+        
+    render(parentelement) {
+        const row = document.createElement('tr') // sor létrehozá
+
+        const firstn1 = document.createElement('td') //cella létrehozása
+        firstn1.innerHTML = this.firstname1
+
+        const firstn2 = document.createElement('td') //cella létrehozása
+        firstn2.innerHTML = this.firstname2
+
+        const lastname1 = document.createElement('td') //cella létrehozása
+        lastname1.innerHTML = this.lastname
+        row.appendChild(lastname1)
+        row.appendChild(firstn1)
+       
+
+        if(this.firstname2 == undefined)
+            firstn1.colSpan = 2
+        else{
+            const firstn2 = document.createElement('td') //cella létrehozása
+            firstn2.innerHTML = this.firstname2
+            row.appendChild(firstn2)
+        }
+        parentelement.appendChild(row)
+        
+    }
+
+}
+
+function innit()
+{
+    const tbody = document.getElementById('tbodyId')
+    for(const nevek of array) {
+        
+        const ember = new Person(nevek)
+
+        ember.render(tbody);
+    }
+}
+
+innit()
