757fb5fe3e17735c16621f10f6ae4ae05e58be14
Meszaros Nandor Farkas <meszaros.nandor1@BOLYAI.LOCAL>
2025-01-31 12:56:03 +0100

    orai kalandozasok

--- datamanager/scripts.js
+++ datamanager/scripts.js
@@ -22,7 +22,7 @@ class DataManager
      */
     constructor(tomb = []) {
         this.#array = tomb
-        this.#updatecallback = () => {}
+        this.#updateCallBack = () => {}
 
         
     }
@@ -32,6 +32,7 @@ class DataManager
      */
     setupdatecallback(updateCallback) {
         this.#updateCallBack = updateCallback
+        this.#updateCallBack(this.#array)
     
     }
 
@@ -56,9 +57,10 @@ class DataManager
                 result.push(element.nev)
             }
          
-        } 
-        this.
+        
+        }
     }
+
 
     
     
@@ -74,29 +76,52 @@ class DataManager
 
             }
             result.push(element.eletkor)
-            this.#updateCallBack(result)
+          
+    }
+    this.#updateCallBack(result)
     }
 
-
-/**
- * @type {Person[]}
- */
-
-#array
-/**
- * @type {updateCallback}
- */
-#updatecallback
+    
 }
 
 
-
-
-
-
-
-
-class datatable 
+class Datatable 
 {
+    /**
+     * 
+     * @param {DataManager} dataManager 
+     */
+    constructor(dataManager){
+        const table = document.createElement('table')
+        document.body.appendChild(table)
+
+        const tbody = document.createElement('tbody')
+        table.appendChild(tbody)
+
+        dataManager.setupdatecallback(person  => {
+            tbody.innerHTML = ""
+      
+            for(const element of person) {
+                const tr = document.createElement('tr')
+
+                const tdnev = document.createElement('td')
+                tdnev.innerHTML = element.nev
+                tr.appendChild(tdnev)
+
+                const tdkor = document.createElement('td')
+                tdkor.innerHTML = element.eletkor             
+                tr.appendChild(tdkor)
+
+                tbody.appendChild(tr)
+            }
+            
+        })
+      
+    
+    }
 
 }
+
+const pelda1 = new DataManager([{nev:"Feri", eletkor: 17}, {nev:"Feri", eletkor: 18}, {nev:"Gábor", eletkor: 17}])
+
+const table = new Datatable(pelda1)
